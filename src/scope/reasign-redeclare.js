f1b81224c94536617497e5be3242dc78b9bd9dd0
Daniel Socorro <daniel.socorro.c@gmail.com>
2022-09-12 10:57:48 -0300

    Rellocation and redeclaration

---
+++ src/scope/reasign-redeclare.js
@@ -0,0 +1,29 @@
+var firstName; // Declaring without assigning is Undefined
+firstName = 'Dan'; // assigning
+console.log(firstName);
+
+var lastName = 'Socorro'; // declaring | assigning
+lastName = 'Castellano'; // re-assigning
+console.log(lastName);
+
+var secondName = 'Alberto';  // declaring | assigning
+var secondName = 'Daniel'; // re-declaring & re-assigning
+console.log(secondName);
+
+//let 
+
+let fruit = 'Apple'; // declaring | assigning
+fruit = 'Kiwi'; // re-asigning
+console.log(fruit);
+
+let fruit = 'Banana'; // Cannot redeclare block-scoped variable 'fruit'.
+
+// const
+
+const animal = 'dog'; // declaring | assigning
+animal = 'cat';  // re-asigning
+console.log(animal);/*  TypeError: Assignment to constant variable. */
+const animal = 'cat'; // Cannot redeclare block-scoped variable 'animal'
+
+const cars = [];
+cars.push("🚗")
