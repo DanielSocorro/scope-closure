65d6727ae344651fad9baa7305840362cf62152e
Daniel Socorro <daniel.socorro.c@gmail.com>
2022-09-12 13:39:47 -0300

    Hoisting

---
+++ src/closure/index.js
@@ -0,0 +1,15 @@
+/* var nameOfDog; // Undefined
+
+console.log(nameOfDog);
+var nameOfDog = 'Warren';
+console.log(nameOfDog); */
+
+
+nameOfDog();
+
+function nameOfDog() {
+    console.log(`Best doggy is: ${Warren}`);
+
+}
+
+var Warren = 'Warreiny';
