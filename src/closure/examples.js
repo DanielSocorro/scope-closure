ce6dcc34925b9222c8366598c0235757740f2946
Daniel Socorro <daniel.socorro.c@gmail.com>
2022-09-12 12:35:41 -0300

    Closures

---
+++ src/closure/examples.js
@@ -0,0 +1,3 @@
+console.log('%c Error: user not register', 'background: red; color: white;')
+console.log('%c Warn: user without address', 'background: yellow; color: blue;')
+console.log('%c Success: user register successfully', 'background: green; color: white;')
